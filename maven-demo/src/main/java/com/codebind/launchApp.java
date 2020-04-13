e2189f6bb17c1f02ab328e5ee6a2febbd7a2457c
ManEipAay39 <connectmanas39@gmail.com>
2020-04-14 01:28:28 +0530

    onemorecommit1

--- maven-demo/src/main/java/com/codebind/launchApp.java
+++ maven-demo/src/main/java/com/codebind/launchApp.java
@@ -10,7 +10,7 @@ public class launchApp {
 		  WebDriver driver=new FirefoxDriver();
 		  driver.get("http://facebook.com");
 		  driver.manage().window().maximize();
-		  System.out.println("ManEipAay");
+		  System.out.println("ManEipAayonemoreedit");
 
 	}
 
